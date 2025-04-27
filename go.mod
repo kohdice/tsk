3e3b28e67179eaf27b9f99b5d3253c7c98d829be
kohdice <kohdice@gmail.com>
2025-04-27 21:23:08 +0900

    chore: Add urfave/cli to the dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kohdice/tsk
 
 go 1.24.2
+
+require github.com/urfave/cli/v3 v3.2.0
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
 
 var version = "0.0.1"
 
 func main() {
-	fmt.Printf("version: %s\n", version)
+	cmd := &cli.Command{
+		Name:    "tsk",
+		Version: version,
+		Usage:   "Scan directories for manifest files and list executable commands",
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			fmt.Println("Hello from tsk!")
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
